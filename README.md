d603d8552a72ea1d24a8a1d4ca4c78f517c5c22f
xhitv <58999952+xhitv@users.noreply.github.com>
2021-07-19 17:49:01 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,149 @@
+# N1-OpenWRT
+Auto Build OpenWRT for N1 and Create Dockerfile by Flippy scripts then upload to docker hub.
+
+
+## 固件包含的插件
+```ini
+┌ Hello World           [ 支持多种主流协议和多种自定义视频分流服务，并直观的显示节点信息 ]  
+├ DNSFilter             [ 支持 AdGuardHome/Host/DNSMASQ/Domain 规则, 对广告进行过滤 ]
+├ KMS Activator         [ KMS 激活服务器，可用于激活 Windows 或 Office ]  
+└ NeteaseMusic          [ 解锁网易云音乐灰色歌曲 ]
+```
+
+
+
+
+## 宿主机以及 Docker 相关设置
+- 开启网卡混杂模式
+```ini
+ip link set eth0 promisc on
+```
+- 创建 `Docker` 虚拟网络  
+
+> 虚拟网络名称为 `macnet`，驱动为 `macvlan` 模式  
+> 
+> 将 `subnet 192.168.50.0`  修改为你自己主路由的网段  
+> 
+> 将 `geteway 192.168.50.1` 修改为你自己的主路由网关
+```ini
+docker network create -d macvlan --subnet=192.168.50.0/24 --gateway=192.168.50.1 -o parent=eth0 macnet
+```
+
+- 拉取镜像配置容器
+> 将 `--ip 192.168.50.9` 修改为你自己主路由的网段IP
+```ini
+# 拉取 `OpenWrt` 镜像
+docker pull itvro/openwrt:phicomm
+
+# 启动 `OpenWrt` 容器
+docker run --restart always --name openwrt -d --network macnet --ip 192.168.50.2 --privileged itvro/openwrt:phicomm /sbin/init
+```
+
+- 修改容器网络配置
+```ini
+# 查看运行中的容器
+docker ps -a
+# CONTAINER ID   IMAGE           COMMAND        CREATED         STATUS        PORTS     NAMES
+# 92c578e3945d   itvro/openwrt   "/sbin/init"   10 minutes ago   Up 9 minutes           openwrt
+
+# 进入容器命令行
+docker exec -it openwrt /bin/bash
+
+# 修改网络配置
+vi /etc/config/network
+```
+```ini
+[按下键盘 Ins 键切至输入状态]
+
+config interface 'loopback'
+	option ifname 'lo'
+	option proto 'static'
+	option ipaddr '127.0.0.1'
+	option netmask '255.0.0.0'
+
+config globals 'globals'
+
+config interface 'lan'
+	option ifname 'eth0'
+	option _orig_ifname 'eth0'
+	option _orig_bridge 'true'
+	option proto 'static'
+	option ipaddr '192.168.50.2'  # 填写创建容器时的IP
+	option netmask '255.255.255.0'
+	option gateway '192.168.50.1'  # 修改为你自己主路由的IP
+	option dns '192.168.50.1'      # DNS 可填主路由IP 也可填公共DNS
+
+[按下键盘 Esc 键退出输入状态，英文键盘输入 :wq 回车 保存退出]
+```
+```ini
+# 重启容器网络服务
+/etc/init.d/network restart
+
+# 退出容器
+exit
+```
+
+- 永久开启网卡混杂模式
+```ini
+# 修改配置
+vi /etc/network/interfaces
+```
+```ini
+[按下键盘 Ins 键切至输入状态]
+
+source /etc/network/interfaces.d/*
+
+# Wired adapter #1
+allow-hotplug eth0
+up ip link set eth0 promisc on  # 添加这一行代码
+no-auto-down eth0
+iface eth0 inet dhcp
+......
+
+[按下键盘 Esc 键退出输入状态，英文键盘输入 :wq 回车 保存退出]
+```
+
+## 容器 OpenWrt 的相关设置
+
+- 接口
+
+> `网络` > `接口` > `修改`  
+> 
+> `忽略此接口` ☑️ > `保存&应用` 
+
+- 防火墙
+
+> `网络` > `防火墙`  
+> 
+> `基本设置` > `启用FullCone-NAT`☑️ > `转发` - `接受`  > `保存&应用`  
+> 
+>  `自定义规则` >  `复制粘贴下列代码` > `保存&应用` 
+```ini
+# 国内慢速或无法联网则添加此条命令
+iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE
+```
+
+- 网络加速
+
+> `网络` > `网络加速`  
+> 
+> `启用 BBR` ☑️ > `保存&应用` 
+
+
+## 旁路网关的相关设置
+
+- 个体设备 
+
+> 设置 `旁路由` 为网关，个别设备独自通过 `旁路由` 上网
+> 
+> 优点：折腾旁路由时，不影响其他设备网络  
+> 
+> 缺点：重复设置每个设备  
+
+- 所有设备 
+
+>  `路由器` 设置 `旁路由` 为该路由器网关，该路由器下所有内网设备都通过 `旁路由` 上网
+> 
+> 优点：该路由器下所有内网设备都能以旁路由模式上网，无需每台单独设置
+> 
+> 缺点：折腾時可能会影响其他设备网络
---
+++ modify.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+
+# 修改主机名字，把N1-OpenWRT修改你喜欢的就行（不能纯数字或者使用中文）
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='N1-OpenWRT'' package/lean/default-settings/files/zzz-default-settings
+
+# 配置自定义主题
+#
+# > 清除旧版argon主题并拉取最新版
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/community/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config package/community/luci-app-argon-config
+#
+# > 修改默认主题为 Argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+#
+
+# 集成自定义插件
+#
+# > AdguardHome
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome package/community/luci-app-adguardhome
+#
+# > lean ssr plus
+#git clone https://github.com/fw876/helloworld.git package/lean/luci-app-ssr-plus
+#
+# > KoolProxyR
+#git clone https://github.com/jefferymvp/luci-app-koolproxyR package/community/luci-app-koolproxyR
+#
+# Debug kpr adapt aarch64
+#sed -i 's/,arm/,aarch64/' package/luci-app-koolproxyR/Makefile
+#
+# > Dockerman
+#git clone https://github.com/lisaac/luci-lib-docker package/community/luci-lib-docker
+#git clone https://github.com/lisaac/luci-app-dockerman package/community/luci-app-dockerman
+#
+# > DnsFilter
+git clone https://github.com/garypang13/luci-app-dnsfilter package/community/luci-app-dnsfilter
+#
+# > GoWebDav
+#git clone https://github.com/immortalwrt/openwrt-gowebdav package/community/openwrt-gowebdav
+#
+# > HelloWorld (vssr)
+git clone https://github.com/jerrykuku/lua-maxminddb package/community/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr package/community/luci-app-vssr
+#
+# > OpenClash
+#svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/community/luci-app-openclash
+#
+# > Install to emmc script for phicomm n1
+#git clone https://github.com/tuanqing/install-program package/install-program
+
+# 个性化配置修改
+#
+# > Mod
+#sed -i '$i '"sed -i '/luciname/d' /usr/lib/lua/luci/version.lua"'' package/lean/default-settings/files/zzz-default-settings
+#sed -i '$i '"echo 'luciname = \"Limitless\"' >> /usr/lib/lua/luci/version.lua"'' package/lean/default-settings/files/zzz-default-settings
+sed -i '$i '"sed -i '/luciversion/d' /usr/lib/lua/luci/version.lua"'' package/lean/default-settings/files/zzz-default-settings
+sed -i '$i '"echo 'luciversion = \"itvro\"' >> /usr/lib/lua/luci/version.lua"'' package/lean/default-settings/files/zzz-default-settings
+
+# firewall custom
+echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
+
+#
+# > 修改插件位置
+sed -i 's/\"services\"/\"network\"/g' feeds/luci/applications/luci-app-upnp/luasrc/controller/upnp.lua
+#sed -i '/\"NAS\"/d' package/lean/luci-app-rclone/luasrc/controller/rclone.lua
+#sed -i 's/\"nas\"/\"services\"/g' package/lean/luci-app-rclone/luasrc/controller/rclone.lua
+#sed -i '/\"NAS\"/d' package/community/openwrt-gowebdav/luci-app-gowebdav/luasrc/controller/gowebdav.lua
+#sed -i 's/\"nas\"/\"services\"/g' package/community/openwrt-gowebdav/luci-app-gowebdav/luasrc/controller/gowebdav.lua
+#
+# > 修改插件名字
+#sed -i 's/\"Rclone\"/\"RcloneSYNC\"/g' feeds/luci/modules/luci-base/po/zh-cn/base.po
+sed -i 's/\"CPU 使用率（%）\"/\"CPU负载\"/g' feeds/luci/modules/luci-base/po/zh-cn/base.po
+sed -i 's/\"Argon 主题设置\"/\"主题设置\"/g' package/community/luci-app-argon-config/po/zh-cn/argon-config.po
+sed -i 's/\"TTYD 终端\"/\"网页终端\"/g' package/lean/luci-app-ttyd/po/zh-cn/terminal.po
+sed -i 's/\"解锁网易云灰色歌曲\"/\"NeteaseMusic\"/g' package/lean/luci-app-unblockmusic/po/zh-cn/unblockmusic.po
+#sed -i 's/\"KMS 服务器\"/\"KMS Activator\"/g' package/lean/luci-app-vlmcsd/po/zh-cn/vlmcsd.zh-cn.po
+#sed -i 's/\"Turbo ACC 网络加速\"/\"网络加速\"/g' package/lean/luci-app-flowoffload/po/zh-cn/flowoffload.po
+#
